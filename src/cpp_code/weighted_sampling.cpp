40e1238e6ff9331ecdb817372967a955ff3c4576
Michał Startek <mist@mimuw.edu.pl>
2020-03-23 12:31:24 +0100

    Rip out leftover IsoSpec stuff

--- src/cpp_code/weighted_sampling.cpp
+++ src/cpp_code/weighted_sampling.cpp
@@ -1,6 +1,12 @@
-class Sampler
+#include <cstddef>
+#include <vector>
+#include "platform.h"
+#include "mocosMath.h"
+
+class Sampler final
 {
-    IsoLayeredGenerator ILG;
+    const std::vector<double>& probabilities;
+    size_t idx;
     size_t to_sample_left;
     const double precision;
     const double beta_bias;
@@ -9,19 +15,13 @@ class Sampler
     size_t current_count;
 
  public:
-    IsoStochasticGenerator(Iso&& iso, size_t no_molecules, double precision = 0.9999, double beta_bias = 5.0);
+    Sampler(const std::vector<double>& probabilities, size_t to_sample, double precision = 1.0, double beta_bias = 5.0);
 
-    ISOSPEC_FORCE_INLINE size_t count() const { return current_count; };
+    MOCOS_FORCE_INLINE size_t count() const { return current_count; };
 
-    ISOSPEC_FORCE_INLINE double mass() const override final { return ILG.mass(); };
+    MOCOS_FORCE_INLINE size_t index() const { return idx; };
 
-    ISOSPEC_FORCE_INLINE double prob() const override final { return static_cast<double>(count()); };
-
-    ISOSPEC_FORCE_INLINE double lprob() const override final { return log(prob()); };
-
-    ISOSPEC_FORCE_INLINE void get_conf_signature(int* space) const override final { ILG.get_conf_signature(space); };
-
-    ISOSPEC_FORCE_INLINE bool advanceToNextConfiguration() override final
+    MOCOS_FORCE_INLINE bool advanceToNextConfiguration()
     {
         /* This function will be used mainly in very small, tight loops, therefore it makes sense to
          * aggressively inline it, despite its seemingly large body.
@@ -38,13 +38,13 @@ class Sampler
                 // Beta was last
                 current_count = 1;
                 to_sample_left--;
-                ILG.advanceToNextConfiguration();
-                current_prob = ILG.prob();
+                idx++;
+                current_prob = probabilities[idx];
                 confs_prob += current_prob;
                 while(confs_prob <= chasing_prob)
                 {
-                    ILG.advanceToNextConfiguration();
-                    current_prob = ILG.prob();
+                    idx++;
+                    current_prob = probabilities[idx];
                     confs_prob += current_prob;
                 }
                 if(to_sample_left <= 0)
@@ -55,8 +55,8 @@ class Sampler
             {
                 // Binomial was last
                 current_count = 0;
-                ILG.advanceToNextConfiguration();
-                current_prob = ILG.prob();
+                idx++;
+                current_prob = probabilities[idx];
                 confs_prob += current_prob;
                 curr_conf_prob_left = current_prob;
             }
