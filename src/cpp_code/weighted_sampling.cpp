d1df58268c8b7e42d87e625eb9f9ab99ed33e865
Michał Startek <mist@mimuw.edu.pl>
2020-03-23 12:54:15 +0100

    Sampler constructor

---
+++ src/cpp_code/weighted_sampling.cpp
@@ -0,0 +1,11 @@
+#include "weighted_sampling.h"
+
+Sampler::Sampler(const std::vector<double>& _probabilities, size_t to_sample, double _precision, double _beta_bias) :
+probabilities(_probabilities),
+idx(0),
+to_sample_left(to_sample),
+precision(_precision),
+beta_bias(_beta_bias),
+confs_prob(0.0),
+chasing_prob(0.0)
+{}
