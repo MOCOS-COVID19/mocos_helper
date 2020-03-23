0d6ccc0131a6013a69b573751c27036ee1207c14
Michał Startek <mist@mimuw.edu.pl>
2020-03-23 12:20:48 +0100

    Add part of platform.h from IsoSpec

---
+++ src/cpp_code/platform.h
@@ -0,0 +1,19 @@
+// Note: __GNUC__ is defined by clang and gcc
+#ifdef __GNUC__
+#define MOCOS_IMPOSSIBLE(condition) if(condition) __builtin_unreachable();
+#define MOCOS_LIKELY(condition) __builtin_expect(static_cast<bool>(condition), 1)
+#define MOCOS_UNLIKELY(condition) __builtin_expect(static_cast<bool>(condition), 0)
+// For aggressive inlining
+#define MOCOS_FORCE_INLINE __attribute__ ((always_inline)) inline
+#elif defined _MSC_VER
+#define MOCOS_IMPOSSIBLE(condition) __assume(!condition);
+#define MOCOS_LIKELY(condition) condition
+#define MOCOS_UNLIKELY(condition) condition
+#define MOCOS_FORCE_INLINE __forceinline inline
+#else
+#define MOCOS_IMPOSSIBLE(condition)
+#define MOCOS_LIKELY(condition) condition
+#define MOCOS_UNLIKELY(condition) condition
+#define MOCOS_FORCE_INLINE inline
+#endif
+
