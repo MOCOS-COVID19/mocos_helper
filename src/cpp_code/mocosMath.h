0214c21d85484db3273109c7956562c88e66c49f
Michał Startek <mist@mimuw.edu.pl>
2020-03-23 12:41:24 +0100

    Fix

--- src/cpp_code/mocosMath.h
+++ src/cpp_code/mocosMath.h
@@ -8,7 +8,9 @@ inline double rdvariate_beta_1_b(double b, std::mt19937& rgen = random_gen)
     return 1.0 - pow(stdunif(rgen), 1.0/b);
 }
 
-inline size_t rdvariate_binom(size_t tries, double succ_prob, std::mt19937& rgen = random_gen);
+#include "btrd.h"
+
+inline size_t rdvariate_binom(size_t tries, double succ_prob, std::mt19937& rgen = random_gen)
 {
     if (succ_prob >= 1.0)
         return tries;
