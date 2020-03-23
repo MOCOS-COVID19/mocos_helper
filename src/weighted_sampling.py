9d6c2bc52011ecba04b0a30addb374bae29bd924
Michał Startek <mist@mimuw.edu.pl>
2020-03-23 17:30:52 +0100

    Documentation

--- src/weighted_sampling.py
+++ src/weighted_sampling.py
@@ -15,10 +15,24 @@ cppyy.load_library(pkg_resources.resource_filename("mocos_helper", "cpp_code/lib
 
 from cppyy.gbl import Sampler, std
 
-def sample_with_replacement(probabilities, to_sample):
-    if type(probabilities) != std.vector("double"):
-        probabilities = std.vector("double")(probabilities)
-    sampler = Sampler(probabilities, to_sample)
+def sample_with_replacement(weights, to_sample):
+    '''Weighted sampling with replacement
+
+    Given an interable (preferably cppyy's std.vector<double>, others will be converted)
+    samples to_sample indexes, according to the weights. Weights are proportional, 
+    and need not sum to 1.
+
+    Returns list of tuples: (index, number if times selected)
+
+    For example:
+
+    sample_with_replacement([0.0001, 0.9, 0.1, 0.000001], 100) might produce:
+    [(1, 91), (2, 9)]
+    '''
+
+    if type(weights) != std.vector("double"):
+        weights = std.vector("double")(weights)
+    sampler = Sampler(weights, to_sample)
     while sampler.advanceToNextConfiguration():
         yield (sampler.index(), sampler.count())
 
