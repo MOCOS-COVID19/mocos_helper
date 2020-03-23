e0a14301eb80328fa9ff57b67a4c4835280980c1
Michał Startek <mist@mimuw.edu.pl>
2020-03-23 14:25:28 +0100

    Bugfix

--- src/cpp_code/weighted_sampling.cpp
+++ src/cpp_code/weighted_sampling.cpp
@@ -2,7 +2,7 @@
 
 Sampler::Sampler(const std::vector<double>& _probabilities, size_t to_sample, double _precision, double _beta_bias) :
 probabilities(_probabilities),
-idx(0),
+idx(-1),
 to_sample_left(to_sample),
 precision(_precision),
 beta_bias(_beta_bias),
