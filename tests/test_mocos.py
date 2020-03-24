856bb8899f30e87bab9cdadf845a71ffa87f634f
Michał Startek <mist@mimuw.edu.pl>
2020-03-24 15:34:48 +0100

    Add simple tests

---
+++ tests/test_mocos.py
@@ -0,0 +1,22 @@
+import mocos_helper as MH
+
+
+S = list(MH.sample_with_replacement([0.2, 0.6, 0.7, 0.3], 300))
+assert sum(x[1] for x in S) == 300
+
+L = list(range(100))
+
+L1, L2 = MH.randomly_split_list(L, 30)
+
+assert len(L1) == 70
+assert len(L2) == 30
+
+assert sorted(L1+L2) == list(range(100))
+
+S = list(MH.nonreplace_sample(range(100), 30))
+assert len(S) == 30
+assert len(set(S)) == 30
+assert set(S).issubset(set(range(100)))
+
+print("All seems OK!")
+
